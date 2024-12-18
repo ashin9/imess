8a7c1adc0caa5c151fa761a195ad072f099e5c25
ashin9 <xinxing.du@foxmail.com>
2024-12-18 21:24:33 +0800

    ✨ feat: v9: 客户端实现及命令行解析

---
+++ client.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	Conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	// 创建客户端对象
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+
+	// 连接 Server
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net dial err:", err)
+		return nil
+	}
+
+	client.Conn = conn
+
+	// 返回对象
+	return client
+}
+
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器 IP (默认为 127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器 Port (默认为 8888)")
+}
+
+func main() {
+	// 命令行解析
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
+	if client == nil {
+		fmt.Println(">>>> 连接服务器失败...")
+		return
+	}
+	fmt.Println(">>>> 连接服务器成功...")
+
+	// 启动客户端业务
+	select {}
+}
